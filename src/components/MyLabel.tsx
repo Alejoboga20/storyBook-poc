6eac8ba6de06ced53fee0f671e50bdafb37c3e48
Alejoboga20 <alejoboga20@gmail.com>
2021-12-30 11:38:38 -0300

    Documenting component

--- src/components/MyLabel.tsx
+++ src/components/MyLabel.tsx
@@ -1,10 +1,19 @@
 import './MyLabel.css';
 
+/**
+ * Primary UI component for display information
+ */
 export const MyLabel = ({ label = 'No Label', size = 'normal' }: MyLabelProps) => {
 	return <span className={`${size}`}>{label}</span>;
 };
 
 interface MyLabelProps {
+	/**
+	 * This is the content displayed in MyLabel
+	 */
 	label: string;
+	/**
+	 * What size to use
+	 */
 	size: 'normal' | 'h1' | 'h2' | 'h3';
 }
